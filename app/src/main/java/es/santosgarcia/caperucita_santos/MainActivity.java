d3daecb3ada61b0d631435c349c2a584b47ca58c
Miguel Angel Nuñez Aparicio <miguel.angel.nao@gmail.com>
2015-12-01 09:25:40 +0100

    Video implementado, musica de fondo tambien implementada, y realizado el archivo strigs-es

--- app/src/main/java/es/santosgarcia/caperucita_santos/MainActivity.java
+++ app/src/main/java/es/santosgarcia/caperucita_santos/MainActivity.java
@@ -1,6 +1,7 @@
 package es.santosgarcia.caperucita_santos;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer;
+        mediaPlayer = MediaPlayer.create(this,R.raw.musica);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(90, 90);
+        mediaPlayer.start();
 
     }
     public void onClick(View v){
